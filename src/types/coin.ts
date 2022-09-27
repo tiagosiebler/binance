2f0a950fd218842c0b206f9abb77caa457005138
lobobruno <brunolobo.df@gmail.com>
2022-09-27 15:04:11 -0300

    use extend only for string literal

--- src/types/coin.ts
+++ src/types/coin.ts
@@ -1,13 +1,5 @@
-import {
-  FuturesContractType,
-  FuturesSymbolOrderBookTicker,
-  PositionSide,
-} from './futures';
-import {
-  numberInString,
-  OrderSide,
-  SymbolFromPaginatedRequestFromId,
-} from './shared';
+import { FuturesContractType, PositionSide } from './futures';
+import { numberInString, OrderSide } from './shared';
 
 export interface PositionRisk {
   symbol: string;
@@ -36,27 +28,46 @@ export type SymbolOrPair =
   | { pair: string; symbol?: never }
   | { pair?: never; symbol: string };
 
-export interface CoinMSymbolOrderBookTicker
-  extends FuturesSymbolOrderBookTicker {
+export interface CoinMSymbolOrderBookTicker {
+  symbol: string;
   pair: string;
+  bidPrice: numberInString;
+  bidQty: numberInString;
+  askPrice: numberInString;
+  askQty: numberInString;
+  time: number;
 }
 
-export interface CoinMAccountTradeParamsWithPair
-  extends Partial<SymbolFromPaginatedRequestFromId> {
+export interface CoinMPaginatedRequest {
+  fromId?: number;
+  startTime?: number;
+  endTime?: number;
+  limit?: number;
+}
+
+export interface CoinMAccountTradeParamsWithPair extends CoinMPaginatedRequest {
   pair: string;
   symbol?: never;
   fromId?: never;
 }
 
 export interface CoinMAccountTradeParamsWithSymbol
-  extends Partial<SymbolFromPaginatedRequestFromId> {
+  extends CoinMPaginatedRequest {
   symbol: string;
   pair?: never;
 }
 
+export interface CoinMAccountTradeParamsWithFromId
+  extends CoinMPaginatedRequest {
+  fromId: number;
+  startTime?: never;
+  endTime?: never;
+}
+
 export type CoinMAccountTradeParams =
   | CoinMAccountTradeParamsWithSymbol
-  | CoinMAccountTradeParamsWithPair;
+  | CoinMAccountTradeParamsWithPair
+  | CoinMAccountTradeParamsWithFromId;
 
 export interface CoinMPositionTrade {
   symbol: string;
