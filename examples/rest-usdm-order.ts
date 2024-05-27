09529541d660a68bcf54a2fcb70255925eef1e63
Tiago Siebler <tiagosiebler@users.noreply.github.com>
2024-05-27 16:28:22 +0100

    chore(): undo sample change

--- examples/rest-usdm-order.ts
+++ examples/rest-usdm-order.ts
@@ -20,7 +20,7 @@ async function start() {
       symbol: 'BTCUSDT',
       type: 'MARKET',
       quantity: 0.001,
-      newOrderRespType: 'FULL',
+      // newOrderRespType: 'FULL',
     });
 
     console.log('market sell result: ', result);
