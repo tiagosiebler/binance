fa2abc3584b284bb7321e9be94167a331e3a335b
JJ-Cro <bryanadams3312@gmail.com>
2024-12-10 10:34:05 +0100

    chore(): updated the endpoint list, added all endpoints in the examples

--- examples/apidoc/MainClient/getVipLoanAccruedInterest.js
+++ examples/apidoc/MainClient/getVipLoanAccruedInterest.js
@@ -2,7 +2,7 @@ const { MainClient } = require('binance');
 
   // This example shows how to call this Binance API endpoint with either node.js, javascript (js) or typescript (ts) with the npm module "binance" for Binance exchange
   // This Binance API SDK is available on npm via "npm install binance"
-  // ENDPOINT: api/v3/myTrades
+  // ENDPOINT: sapi/v1/loan/vip/accruedInterest
   // METHOD: GET
   // PUBLIC: NO
 
@@ -11,7 +11,7 @@ const client = new MainClient({
   api_secret: 'insert_api_secret_here',
 });
 
-client.getAccountTradeList(params)
+client.getVipLoanAccruedInterest(params)
   .then((response) => {
     console.log(response);
   })
--- examples/apidoc/MainClient/testNewOrder.js
+++ examples/apidoc/MainClient/testNewOrder.js
@@ -2,8 +2,8 @@ const { MainClient } = require('binance');
 
   // This example shows how to call this Binance API endpoint with either node.js, javascript (js) or typescript (ts) with the npm module "binance" for Binance exchange
   // This Binance API SDK is available on npm via "npm install binance"
-  // ENDPOINT: api/v3/myTrades
-  // METHOD: GET
+  // ENDPOINT: api/v3/order/test
+  // METHOD: POST
   // PUBLIC: NO
 
 const client = new MainClient({
@@ -11,7 +11,7 @@ const client = new MainClient({
   api_secret: 'insert_api_secret_here',
 });
 
-client.getAccountTradeList(params)
+client.testNewOrder(params)
   .then((response) => {
     console.log(response);
   })
