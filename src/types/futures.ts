97de50552b03a5a276e0fd99942c316f15e97abc
JJ-Cro <bryanadams3312@gmail.com>
2025-03-13 13:07:01 +0100

    feat(): updated coinm types

--- src/types/futures.ts
+++ src/types/futures.ts
@@ -53,6 +53,7 @@ export interface SymbolKlinePaginatedParams {
 
 export interface FuturesDataPaginatedParams {
   symbol: string;
+  contractType?: string;
   period: '5m' | '15m' | '30m' | '1h' | '2h' | '4h' | '6h' | '12h' | '1d';
   limit?: number;
   startTime?: number;
