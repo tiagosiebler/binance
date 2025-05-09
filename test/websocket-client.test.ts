e69fe2567c3c94534e323953d014f18bd3ad8361
Tiago Siebler <tiagosiebler@users.noreply.github.com>
2025-05-09 11:21:53 +0100

    chore(): fix test

--- test/websocket-client.test.ts
+++ test/websocket-client.test.ts
@@ -5,9 +5,9 @@ describe('websocket-client', () => {
     it('should parse & resolve an event with nested data', () => {
       const event =
         '{"stream":"!forceOrder@arr","data":{"e":"forceOrder","E":1634653599186,"o":{"s":"IOTXUSDT","S":"SELL","o":"LIMIT","f":"IOC","q":"3661","p":"0.06606","ap":"0.06669","X":"FILLED","l":"962","z":"3661","T":1634653599180}}}';
-      const result = parseRawWsMessage(event);
+      const result = parseRawWsMessage({ data: event });
       expect(typeof result).toBe('object');
-      expect(result.data).toBe(undefined);
+      expect(result.stream).toBe('!forceOrder@arr');
     });
   });
 });
