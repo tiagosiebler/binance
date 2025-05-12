94a75ffcbd25a3b21ec1c6dda882a65391f6d2c6
JJ-Cro <bryanadams3312@gmail.com>
2025-05-12 15:34:32 +0200

    feat(): added apidoc examples and endpoint map to wsapi branch

--- examples/apidoc/MainClient/getMarginRiskUserDataListenKey.js
+++ examples/apidoc/MainClient/getMarginRiskUserDataListenKey.js
@@ -2,16 +2,16 @@ const { MainClient } = require('binance');
 
   // This example shows how to call this Binance API endpoint with either node.js, javascript (js) or typescript (ts) with the npm module "binance" for Binance exchange
   // This Binance API SDK is available on npm via "npm install binance"
-  // ENDPOINT: sapi/v1/lending/auto-invest/plan/edit
+  // ENDPOINT: sapi/v1/margin/listen-key
   // METHOD: POST
-  // PUBLIC: NO
+  // PUBLIC: YES
 
 const client = new MainClient({
   api_key: 'insert_api_key_here',
   api_secret: 'insert_api_secret_here',
 });
 
-client.updateAutoInvestmentPlanOld(params)
+client.getMarginRiskUserDataListenKey(params)
   .then((response) => {
     console.log(response);
   })
