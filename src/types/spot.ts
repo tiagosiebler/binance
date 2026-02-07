0d0e143ab20476b2ecee42041f8fe5055ae3fb8e
Tiago Siebler <tiagosiebler@siebly.io>
2026-02-07 11:22:16 +0000

    feat(v3.3.2): add enablePortfolioMarginTrading APIPermission property

--- src/types/spot.ts
+++ src/types/spot.ts
@@ -508,17 +508,18 @@ export interface APITradingStatus {
 export interface APIPermissions {
   ipRestrict: boolean;
   createTime: number;
+  enableReading: boolean;
   enableWithdrawals: boolean; // This option allows you to withdraw via API. You must apply the IP Access Restriction filter in order to enable withdrawals
   enableInternalTransfer: boolean; // This option authorizes this key to transfer funds between your master account and your sub account instantly
+  enableMargin: boolean; //  This option can be adjusted after the Cross Margin account transfer is completed
+  enableFutures: boolean; //  API Key created before your futures account opened does not support futures API service
   permitsUniversalTransfer: boolean; // Authorizes this key to be used for a dedicated universal transfer API to transfer multiple supported currencies. Each business's own transfer API rights are not affected by this authorization
   enableVanillaOptions: boolean; //  Authorizes this key to Vanilla options trading
-  enableReading: boolean;
-  enableFutures: boolean; //  API Key created before your futures account opened does not support futures API service
-  enableMargin: boolean; //  This option can be adjusted after the Cross Margin account transfer is completed
   enableSpotAndMarginTrading: boolean; // Spot and margin trading
   tradingAuthorityExpirationTime: number; // Expiration time for spot and margin trading permission
   enableFixApiTrade: boolean; //  Authorizes this key to use FIX API trading
   enableFixReadOnly: boolean; //  Authorizes this key to use FIX API reading
+  enablePortfolioMarginTrading: true;
 }
 
 export interface AssetDetail {
